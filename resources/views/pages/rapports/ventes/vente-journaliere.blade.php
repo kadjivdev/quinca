a3fbb63ccff83d3ed1e29ebc5a14a2cf69a90c18
kadjivdev <kadjivdev@gmail.com>
2025-06-19 10:06:59 +0100

    revu des ventes journalières

--- resources/views/pages/rapports/ventes/vente-journaliere.blade.php
+++ resources/views/pages/rapports/ventes/vente-journaliere.blade.php
@@ -8,6 +8,7 @@
         color: white !important;
     }
 </style>
+
 <div class="container-fluid px-4 py-4">
     <div class="row">
         <div class="col-12">
@@ -55,7 +56,7 @@
             <!-- Tableau des ventes -->
             <div class="card shadow-sm">
                 <div class="card-header bg-transparent py-3 d-flex justify-content-between align-items-center">
-                    <h5 class="mb-0">Rapport des ventes du {{ Carbon\Carbon::parse(request('date', now()))->format('d/m/Y') }} Légende: <span class="badge bg-light text-dark">Ecritures des Dépôts</span> | <span class="border badge bg-white text-dark">Ecritures de la Direction</span></h5>
+                    <h5 class="mb-0">Rapport des ventes du {{ Carbon\Carbon::parse(request('date', now()))->format('d/m/Y') }} Légende: <span class="badge bg-secondary text-white">Ecritures des Dépôts</span> | <span class="border badge bg-white text-dark">Ecritures de la Direction</span></h5>
                     <!-- <button class="btn btn-sm btn-outline-primary">
                         <i class="fas fa-file-excel me-2"></i>Exporter
                     </button> -->
