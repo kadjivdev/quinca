106cd6f41647418814f29e8c45d744448266250e
kadjivdev <kadjivdev@gmail.com>
2025-12-09 11:05:55 +0100

    regularisation de la facture FAC-20251208-0007 -4

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,6 @@ use App\Models\Vente\Transport;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
