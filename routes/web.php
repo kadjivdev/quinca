0a5bfc7259491aead337ab5f3aef8e632ad7b237
kadjivdev <kadjivdev@gmail.com>
2025-12-17 12:19:11 +0100

    suppression du compte client d'id 11255

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ use App\Models\Achat\FactureFournisseur;
 use App\Models\Catalogue\Inventaire;
 use App\Models\Revendeur\FactureRevendeur;
 use App\Models\Stock\StockDepot;
+use App\Models\Vente\CompteClient;
 use App\Models\Vente\FactureClient;
 use Carbon\Carbon;
 
@@ -40,17 +41,12 @@ use Carbon\Carbon;
 
 // DEBUGGING ROUTES
 Route::get("/debug", function () {
-    $facture = FactureClient::withTrashed()->firstWhere("numero", "_FAC-20251212-0005");
+    $compte = CompteClient::with("accompteClient", "factureClient", "factureRevendeur", "reglementClient", "reglementRevendeur")
+        ->findOrFail(11255);
 
-    $facture->update([
-            "numero" => "FAC-20251212-0005",
-            "validated_by" => null,
-            "reference_recu" => "KAD18907",
-            "deleted_by" => null,
-            "deleted_at" => null
-        ]);
+    $compte->delete();
+    return $compte;
 
-    return $facture;
     // ==== Retrait des factures revendeurs crée le ['2025-12-08 00:00:00' au '2025-12-16 23:59:59'] des inventaires de cette période  ===== //
     // $factureRevs = FactureRevendeur::with("inventaire")->whereBetween('created_at', ['2025-12-08 00:00:00', '2025-12-16 23:59:59'])
     //     ->get();
