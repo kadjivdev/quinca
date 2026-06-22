245c7750fe177e483cfe2803c94e570ba556d339
kadjivdev <kadjivdev@gmail.com>
2026-06-22 09:18:41 +0100

    revu du debugging...

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,8 @@ Route::get("/debug", function () {
 
     // return $livraisonDeleteds;
 
-    $facture = FactureFournisseur::with("lignes.quantite_livree","bonLivraison")->firstWhere("code", "FAC26069810");
+    $facture = FactureFournisseur::with("bonLivraison.lignes")
+        ->firstWhere("code", "FAC26069810");
 
     return $facture;
     // $BonLivraisonFournisseurs = BonLivraisonFournisseur::with("lignes", "lignes.uniteMesure", "lignes.article")
